8391df239e13e87d810ad60e9118a0d8d4f0d463
ask-r0 <75451424+ask-r0@users.noreply.github.com>
2020-12-06 21:42:19 +0100

    Added converter functions.

--- board.cpp
+++ board.cpp
@@ -7,3 +7,15 @@ using namespace std;
 
 enum pieces : short {e = 0, P, p = 1, N, n = 2, B, b = 3, R, r = 4, Q, q = 5,K, k = 6};
 short tenTwelveBoard[120];
+
+
+
+
+short sq120sq64Converter(short n) {
+    short ans =  8 * ((n - 21) / 10) + n % 10 - 1 ;
+    return ans;
+}
+short sq64TOsq120(short n) {
+    short ans = n % 8 + n / 8 * 10 + 21 ;
+    return ans;
+}
