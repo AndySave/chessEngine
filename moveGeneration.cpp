c0d26f5ec8cc01b12927775c1ca6d9449e96e521
AndySave <51698537+AndySave@users.noreply.github.com>
2020-12-08 20:09:59 +0100

    added few thing here and there :)

--- moveGeneration.cpp
+++ moveGeneration.cpp
@@ -1,16 +1,8 @@
 
 #include "defenitions.h"
 
-#define fromsq(m) (m & 0x7F)
-#define tosq(m) ((m >> 7) & 0x7F)
-#define captured(m) ((m >> 14) & 0xF)
-#define promoted(m) ((m >> 20) & 0xF)
-
-#define epFlag(m) 0x40000
-#define pwnStartFlag(m) 0x80000
-#define castleFlag(m) 0x1000000
-
-#define captureFlag(m) 0x7C000
-#define promotedFlag(m) 0xF00000
-
-
+void printMove(int move){
+    cout << fromsq(move) << endl;
+    cout << tosq(move) << endl;
+    cout << promoted(move) << endl;
+}
